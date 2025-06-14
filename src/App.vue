92c0aa755c90a8950f9cfda809afa17bce5aeb02
Muhamad Tabina Widyatna <muhamadtabina12@gmail.com>
2025-06-14 13:44:53 +0700

    add components header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
+  <Header />
   <main class="mx-auto min-h-[80vh]">
     <RouterView />
   </main>
+  <Footer />
 </template>
